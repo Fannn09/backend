9219c2a1ed85bac2f6fc55b1313f3144a6872cf5
Fahmiawaludin <fahmi.123awaludin@gmail.com>
2023-04-11 19:21:23 +0700

    combo box

--- app/Http/Controllers/DepartementController.php
+++ app/Http/Controllers/DepartementController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Departements;
 use Illuminate\Http\Request;
-use App\Models\Positions;
+use App\Models\User;
 
 class DepartementController extends Controller
 {
@@ -18,7 +18,7 @@ class DepartementController extends Controller
     public function create()
     {
         $title = "Tambah data";
-        $managers = User::where('alias', 'Manager')->get();
+        $managers = User::where('position', 'manager')->get();
         return view('departements.create', compact('managers', 'title'));
     }
 
@@ -46,7 +46,7 @@ class DepartementController extends Controller
     public function edit(Departements $departement)
     {
         $title = "Edit Data departement";
-        $managers = User::where('alias', 'Manager')->get();
+        $managers = User::where('position', 'manager')->get();
         return view('departements.edit', compact('departement', 'managers', 'title'));
     }
 
