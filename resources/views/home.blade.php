f0579cd1e29d68b0b3dc18955284c10cc7f381fa
Fahmiawaludin <fahmi.123awaludin@gmail.com>
2023-04-18 06:07:23 +0700

    ngebenerin_navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -128,19 +128,19 @@
                     </div>
                   </nav>
                   <div class="container-fluid">
-                    <div class="card">
+                    <!-- <div class="card">
                       <div class="card-body">
                         <h1 class="crad-title">@yield('title', $title)</h1>
                         @yield('content')
                       </div>
-                    </div>
+                    </div> -->
                     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
 
                   </div>
-                  <div class="header-right-btn f-right d-none d-lg-block ml-30">
+                  <!-- <div class="header-right-btn f-right d-none d-lg-block ml-30">
                     <a href="#" class="btn header-btn">01654.066.456</a>
-                  </div>
+                  </div> -->
                 </div>
               </div>
               <!-- Mobile Menu -->
